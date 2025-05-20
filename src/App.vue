2f50bd730149c5153cd8fbe03da427d846e756fe
Nobody-IArc <utk3970@naver.com>
2025-05-20 15:58:23 +0900

    기본 레이아웃 구성 완료

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-
+<div class="app">
+  <div class="container py-4">
+    <header class="pb-4 mb-4 border-bottom">
+      <router-link to="/" class="text-dark text-decoration-none">
+        <b class="fs-4">Memo Application</b>
+      </router-link>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer class="pt-4 mt-4 border-top">&copy; 2025. Memo. All rights reserved.</footer>
+  </div>
+</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,60 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue';
+import { StorageService } from '@/services/StorageService.js';
 
-<template></template>
+// 스토리지 서비스 생성
+const storageService = new StorageService('myMemo');
 
-<style lang="scss" scoped></style>
+// 반응형 상태
+const state = reactive({
+  memos: []
+});
+
+// 커스텀 생성 훅
+(async function onCreated() {
+  state.memos = storageService.getItems();
+})();
+</script>
+
+<template>
+  <div class="memo-list">
+    <router-link :to="`/memos/${m.id}`" class="item" v-for="m in state.memos">
+      <div class="d-flex pt-3">
+        <div class="pb-3 mb-0 w-100">
+          <div class="d-flex justify-content-between">
+            <b>{{ m.title }}</b>
+            <div>
+              <span role="button">DELETE</span>
+            </div>
+          </div>
+          <div class="mt-2">{{ m.content }}</div>
+        </div>
+      </div>
+    </router-link>
+    <router-link to="/memos/add" class="add btn btn-light">ADD</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.memo-list {
+  .item {
+    background: #f8f9fa;
+    border: 1px solid #eee;
+    display: block;
+    color: #000;
+    text-decoration: none;
+    padding: 20px 30px;
+    margin: 15px 0;
+
+    &:hover {
+      border-color: #aaa;
+    }
+  }
+
+  .add {
+    display: block;
+    padding: 25px;
+    border: 1px solid #eee;
+  }
+}
+</style>
